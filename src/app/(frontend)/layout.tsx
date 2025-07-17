fea0867e7ff4dc922bb25b0d312f2d7b2ba421aa
agb220 <annabalabonchik@gmail.com>
2025-07-17 16:02:45 +0300

    add favicon

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -26,6 +26,14 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en" className={`${poppins.variable} ${open_sans.variable}`}>
+      <head>
+        <link rel="icon" type="image/png" href="/images/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/images/favicon.svg" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/images/site.webmanifest" />
+      </head>
       <body>
         <main>{children}</main>
       </body>
