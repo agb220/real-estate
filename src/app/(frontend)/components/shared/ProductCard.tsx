0c7ca3e685640cd47f84eae3c8f734e7b7a1c432
agb220 <annabalabonchik@gmail.com>
2025-08-19 19:29:04 +0300

    update price

--- src/app/(frontend)/components/shared/ProductCard.tsx
+++ src/app/(frontend)/components/shared/ProductCard.tsx
@@ -28,7 +28,7 @@ const ProductCard = ({ product }: ProductCardProps) => {
           <h3 className="product-card__name">
             <Link href={`/offers/${product.slug}`}>{product.title}</Link>
           </h3>
-          <p className="product-card__price">{product.productDetails.prices.Payment}</p>
+          <p className="product-card__price">{product.productDetails.prices.fullPrice}</p>
           <p className="product-card__location">
             {typeof product.main.location === 'object' && product.main.location !== null
               ? product.main.location.name
