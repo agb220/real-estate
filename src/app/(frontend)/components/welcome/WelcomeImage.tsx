da59c416a0b3e93e129bbabc7b26ef0111f11d13
agb220 <annabalabonchik@gmail.com>
2025-09-17 15:52:21 +0300

    update welcome section

---
+++ src/app/(frontend)/components/welcome/WelcomeImage.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import { getImageUrl } from '@/utilities/getUrl'
+
+interface WelcomeImageProps {
+  mainImage: any
+  title: string
+}
+
+export default async function WelcomeImage({ mainImage, title }: WelcomeImageProps) {
+  return (
+    <div className="imgs-block__wrapper img-block">
+      <Image
+        src={getImageUrl({
+          media: mainImage,
+          size: 'pngBig',
+        })}
+        sizes="(max-width: 768px) 100vw, 50vw"
+        height={495}
+        width={490}
+        alt={title}
+        className="img-block__main"
+        loading="lazy"
+      />
+    </div>
+  )
+}
